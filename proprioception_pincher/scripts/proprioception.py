2c53fdd9161fd0c714f5530db57b3cd55c0f193e
altair <q.houbre@gmail.com>
2023-03-02 15:34:08 +0200

    sending EE pose

--- proprioception_pincher/scripts/proprioception.py
+++ proprioception_pincher/scripts/proprioception.py
@@ -19,28 +19,40 @@ import os.path
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from sensor_msgs.msg import JointState
+from interbotix_xs_modules.arm import InterbotixManipulatorXS
+import interbotix_common_modules.angle_manipulation as ang
 
 class Proprioception(object):
   def __init__(self):
     super(Proprioception, self).__init__()
-    moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('proprioception', anonymous=True)
+    #moveit_commander.roscpp_initialize(sys.argv)
+    #rospy.init_node('proprioception', anonymous=True)
+    self.bot = InterbotixManipulatorXS("px150", "arm", "gripper")
     self.pub = rospy.Publisher('/proprioception/joint_states', JointState, queue_size=10)
+    self.pub_ee = rospy.Publisher('/proprioception/EE', Pose, queue_size=1)
     rospy.Subscriber('/px150/joint_states', JointState, self.joint_states)
     self.js = JointState()
 
-    
   def joint_states(self,msg):
     self.js = msg
 
   def publishJS(self):
     self.pub.publish(self.js)
 
+  def publishEE(self):
+    p = Pose()
+    t = self.bot.arm.get_ee_pose()
+    p.position.x = round(t[0,3],2)
+    p.position.y = round(t[1,3],2)
+    p.position.z = round(t[2,3],2)
+    self.pub_ee.publish(p)
+
 if __name__ == '__main__':
   try:
     prop = Proprioception()
-    rate = rospy.Rate(5)
+    rate = rospy.Rate(10)
     while not rospy.is_shutdown():
+        prop.publishEE()
         prop.publishJS()
         rate.sleep()
 
