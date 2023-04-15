e2c9d8d98e7e08d40abfc28c6a96bb2b5b550850
altair <q.houbre@gmail.com>
2023-04-15 08:04:19 +0300

    possible version

--- display_points/scripts/display_pts.py
+++ display_points/scripts/display_pts.py
@@ -25,3 +25,24 @@ if __name__ == '__main__':
    d = DisplayPts()
    d.display()
    rospy.spin()
+
+
+
+   import matplotlib.pyplot as plt
+import matplotlib.animation
+import numpy as np
+
+fig, ax = plt.subplots()
+x, y = [],[]
+sc = ax.scatter(x,y)
+plt.xlim(0,10)
+plt.ylim(0,10)
+
+def animate(i):
+    x.append(np.random.rand(1)*10)
+    y.append(np.random.rand(1)*10)
+    sc.set_offsets(np.c_[x,y])
+
+ani = matplotlib.animation.FuncAnimation(fig, animate, 
+                frames=2, interval=100, repeat=True) 
+plt.show()
