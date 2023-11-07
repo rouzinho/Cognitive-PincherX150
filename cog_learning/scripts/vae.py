348854dfba0979ba373f9e143aace34d2666fb29
altair <q.houbre@gmail.com>
2023-11-07 14:12:14 +0200

    test successfull with 4 samples

--- cog_learning/scripts/vae.py
+++ cog_learning/scripts/vae.py
@@ -33,6 +33,7 @@ class Sampling(nn.Module):
       #batch, dim = z_mean.shape
       # generate a normal random tensor (epsilon) with the same shape as z_mean
       # this tensor will be used for reparameterization trick
+      #print(z_mean.shape)
       epsilon = Normal(0, 1).sample(z_mean.shape).to(z_mean.device)
       # apply the reparameterization trick to generate the samples in the
       # latent space
@@ -41,39 +42,46 @@ class Sampling(nn.Module):
 class VariationalEncoder(nn.Module):
    def __init__(self, latent_dims):
       super(VariationalEncoder, self).__init__()
-      self.linear1 = nn.Linear(5, 3)
-      #self.linear2 = nn.Linear(4, 3)
-      self.linear2 = nn.Linear(3, latent_dims)
-      self.linear3 = nn.Linear(3, latent_dims)
-      self.N = torch.distributions.Normal(0, 1)
-      self.kl = 0
+      self.linear1 = nn.Linear(8, 6)
+      self.linear2 = nn.Linear(6, 4)
+      self.linear3 = nn.Linear(4, 3)
+      self.linear4 = nn.Linear(3, latent_dims)
+      self.linear5 = nn.Linear(3, latent_dims)
+      #self.N = torch.distributions.Normal(0, 1)
+      #self.kl = 0
       self.sampling = Sampling()
 
    def forward(self, x):
       #x = torch.flatten(x, start_dim=1)
-      x = torch.tanh(self.linear1(x))
-      #x = torch.tanh(self.linear2(x))
-      #z_mean = torch.tanh(self.linear3(x))
-      #z_log_var = torch.tanh(self.linear4(x))
-      mu =  self.linear2(x)
-      sigma = torch.exp(self.linear3(x))
-      z = mu + sigma*self.N.sample(mu.shape)
-      self.kl = -0.5 * torch.sum(1 + sigma - mu.pow(2) - sigma.exp())
-      return z
+      x = torch.relu(self.linear1(x))
+      x = torch.relu(self.linear2(x))
+      x = torch.relu(self.linear3(x))
+      z_mean = self.linear4(x)
+      z_log_var = self.linear5(x)
+      #mu =  torch.tanh(self.linear2(x))
+      #sigma = torch.exp(self.linear3(x))
+      #z = mu + sigma*self.N.sample(mu.shape)
+      #self.kl = -0.5 * torch.sum(1 + sigma - mu.pow(2) - sigma.exp())
+      #return z
       #z = self.sampling(z_mean, z_log_var)
-      #return z_mean, z_log_var, z
+      epsilon = torch.randn_like(z_mean)
+      z_reparametrized = z_mean + z_log_var*epsilon
+      return z_mean, z_log_var, z_reparametrized
 
 class Decoder(nn.Module):
    def __init__(self, latent_dims):
       super(Decoder, self).__init__()
       self.linear1 = nn.Linear(latent_dims, 3)
-      self.linear2 = nn.Linear(3, 5)
-      #self.linear3 = nn.Linear(4, 5)
+      self.linear2 = nn.Linear(3, 4)
+      self.linear3 = nn.Linear(4, 6)
+      self.linear4 = nn.Linear(6, 8)
 
    def forward(self, z):
       z = torch.tanh(self.linear1(z)) #F.relu
-      #z = torch.tanh(self.linear2(z))
-      z = torch.sigmoid(self.linear2(z))
+      z = torch.tanh(self.linear2(z))
+      z = torch.tanh(self.linear3(z))
+      z = self.linear4(z)
+      #z = torch.tanh(self.linear4(z))
       return z
       #return z.reshape((-1, 1, 28, 28))
 
@@ -84,14 +92,14 @@ class VariationalAutoencoder(nn.Module):
       self.decoder = Decoder(latent_dims)
 
    def forward(self, x):
-      z = self.encoder(x)
-      return self.decoder(z)
-      #z_mean, z_log_var, z = self.encoder(x)
+      #z = self.encoder(x)
+      #return self.decoder(z)
+      z_mean, z_log_var, z = self.encoder(x)
       # pass the latent vector through the decoder to get the reconstructed
       # image
-      #reconstruction = self.decoder(z)
+      reconstruction = self.decoder(z)
       # return the mean, log variance and the reconstructed image
-      #return z_mean, z_log_var, reconstruction
+      return z_mean, z_log_var, reconstruction
 
 class Habituation(object):
    def __init__(self):
@@ -102,23 +110,23 @@ class Habituation(object):
       # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
       # https://arxiv.org/abs/1312.6114
       KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-      return KLD.mean()
-      #return KLD
+      #return KLD.mean()
+      return KLD
 
    def reconstruction_loss(self, x_reconstructed, x):
       mse_loss = nn.MSELoss()
       return mse_loss(x_reconstructed, x)
-      #return bce_loss(x_reconstructed, x)
 
    def vae_loss(self, y_pred, y_true):
       mu, logvar, recon_x = y_pred
       self.recon_loss = self.reconstruction_loss(recon_x, y_true)
       self.kld_loss = self.vae_gaussian_kl_loss(mu, logvar)
-      return  self.recon_loss + self.kld_loss
+      return 100 * self.recon_loss + self.kld_loss
 
    def train(self, data, epochs=6000):
       kl_weight = 0.8
-      opt = torch.optim.Adam(list(self.vae.encoder.parameters()) + list(self.vae.decoder.parameters()), lr=0.01)
+      #opt = torch.optim.Adam(list(self.vae.encoder.parameters()) + list(self.vae.decoder.parameters()), lr=0.001)
+      opt = torch.optim.Adam(self.vae.parameters(), lr=0.01)
       train_loss = 0.0
       last_loss = 10
       stop = False
@@ -127,31 +135,32 @@ class Habituation(object):
       #for epoch in range(epochs):
       while not stop:
          for sample, y in data:
+            self.vae.train()
             s = sample.to(device) # GPU
             opt.zero_grad()
-            x_hat = self.vae(s)
+            #x_hat = self.vae(s)
             #      loss = ((s - x_hat)**2).sum() + autoencoder.encoder.kl
-            loss_mse = ((s - x_hat)**2).sum() 
+            #loss_mse = ((s - x_hat)**2).sum() 
             # KL divergence between encoder distrib. and N(0,1) distrib. 
-            loss_kl = self.vae.encoder.kl 
+            #loss_kl = self.vae.encoder.kl 
             # Get weighted loss
-            loss = (loss_mse * (1 - kl_weight) + loss_kl * kl_weight)
-            train_loss += loss.item()
-            print("loss reconstruct : ",loss_mse)
+            #loss = (loss_mse * (1 - kl_weight) + loss_kl * kl_weight)
+            #train_loss += loss.item()
+            #print("loss reconstruct : ",loss_mse)
             #print("loss KL : ",loss_kl)
             #print("loss total : ",loss)
             
-            #pred = self.vae(s)
-            #loss = self.vae_loss(pred, s)
-            #print("loss reconstruct : ",self.recon_loss)
-            #print("loss KL : ",self.kld_loss)
+            pred = self.vae(s)
+            loss = self.vae_loss(pred, s)
+            print("loss reconstruct : ",self.recon_loss)
+            print("loss KL : ",self.kld_loss)
             #print("loss total : ",loss)
             loss.backward()
             opt.step()
             i += 1
-            if loss_mse < 0.0001:
+            if self.kld_loss < 0.005 and self.recon_loss < 0.0001:
                stop = True
-            last_loss = loss_mse
+            #last_loss = loss_mse
             #if nb_data > 1:
             #   if loss_mse < 9e-5 and loss_kl < 9e-5:
             #      stop = True
@@ -162,11 +171,11 @@ class Habituation(object):
 
    def plot_latent(self, dataset, num_batches=100):
       colors = []
-      for i in range(0,10):
+      for i in range(0,5):
          for sample, col in dataset:
-               z = self.vae.encoder(sample)
-               z = z.to('cpu').detach().numpy()
+               z, z_log, recon = self.vae.encoder(sample)
                print(z)
+               z = z.to('cpu').detach().numpy()
                plt.scatter(z[0], z[1], c=col, cmap='tab10')
 
 if __name__ == "__main__":
@@ -179,10 +188,10 @@ if __name__ == "__main__":
    #            download=True),
    #     batch_size=128,
    #     shuffle=True)
-   goal = [0.1,0.1,0.5,0.2,0.3]
-   sec_goal = [0.4,0.4,0.1,0.6,0.1]
-   third_goal = [0.2,0.34,0.12,0.43,0.8]
-   test_goal = [0.1,0.1,0.5,0.3,0.3]
+   goal = [0.1,0.1,0.5,0.2,0.3,0.25,0.65,0.9]
+   sec_goal = [0.4,0.4,0.1,0.6,0.1,0.6,0.1,0.45]
+   third_goal = [0.2,0.34,0.12,0.43,0.8,0.85,0.45,0.76]
+   test_goal = [0.1,0.1,0.5,0.3,0.3,0.2,0.9,0.35]
    tensor_goal = torch.tensor(goal,dtype=torch.float)
    tensor_secgoal = torch.tensor(sec_goal,dtype=torch.float)
    tensor_rdgoal = torch.tensor(third_goal,dtype=torch.float)
@@ -194,8 +203,8 @@ if __name__ == "__main__":
    data.append([tensor_goal,c_r])
    #vae = VariationalAutoencoder(latent_dims).to(device) # GPU
    data.append([tensor_secgoal,c_b])
-   #data.append([tensor_rdgoal,c_g])
-   #data.append([tensor_test,c_y])
+   data.append([tensor_rdgoal,c_g])
+   data.append([tensor_test,c_y])
    habit = Habituation()
    habit.train(data)
    res = habit.vae.forward(tensor_goal)
